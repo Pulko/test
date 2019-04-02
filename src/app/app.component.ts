16288097460ca32a4a332644648bfd3060f9917c
pulko <vzdbovich@gmail.com>
2019-04-03 02:33:55 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rtb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rtb-test';
+}
